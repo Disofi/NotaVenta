58c88d12a95857e7d2d6847a226fbb4be3cef2cb
Yerko <yvegaq@gmail.com>
2019-11-29 13:10:30 -0300

    Bugfix
    Se realiza bugfix de saldos

--- NotaVenta/Controllers/VentaController.cs
+++ NotaVenta/Controllers/VentaController.cs
@@ -1304,14 +1304,14 @@ namespace NotaVenta.Controllers
         }
 
         [HttpPost]
-        public JsonResult ObtenerSaldo(string RutAuxiliar)
+        public JsonResult ObtenerSaldo(string RutAuxiliar, string CodAux)
         {
             double MontoTotal = 0;
             double Saldo = 0;
             try
             {
                 List<SaldosModel> Saldos = new List<SaldosModel>();
-                Saldos = controlDisofi().ObtenerSaldo(RutAuxiliar, baseDatosUsuario());
+                Saldos = controlDisofi().ObtenerSaldo(RutAuxiliar, CodAux, baseDatosUsuario());
 
                 foreach (SaldosModel item in Saldos)
                 {
@@ -1326,8 +1326,6 @@ namespace NotaVenta.Controllers
 
                 throw;
             }
-
-
         }
     }
 }
